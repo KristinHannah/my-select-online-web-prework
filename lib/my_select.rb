82e9fd13b668e112cb807b8853921eb93f566990
Kristin Kraemer <kristinhannahk@gmail.com>
2019-04-23 14:16:09 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(array)
   result = []
  while i < array.length
     test = yield(array[i])
-    if test == "true"
+    if test == TRUE
       result << array[i]
     i = i + 1 
   break if i == array.length
