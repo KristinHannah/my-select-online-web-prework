5e429b4b57bf9b8830176bc812a23fc106a4e22d
Kristin Kraemer <kristinhannahk@gmail.com>
2019-04-23 14:12:51 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,7 @@
 def my_select(collection)
   i = 0 
  while i < collection.length
-    test = yield collection[i]
+    test = yield(collection[i])
     if test == "true"
       puts "#{collection[i]}"
   i += 1 
